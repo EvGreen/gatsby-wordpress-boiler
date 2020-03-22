1cd1575ebd8c51af2ff68d43c86e32e88dce07d6
Daniel Leczycki <the242@gmail.com>
2020-03-22 20:14:11 +0100

    nn

--- src/templates/page.js
+++ src/templates/page.js
@@ -6,7 +6,9 @@ export default ({ data }) => {
 
   return (
     <div>
-		<h2>{page.slug}</h2>
+	  <h2>{page.title}</h2>
+	  <h2>{page.id}</h2>
+	  <h2>{page.slug}</h2>
       <p>Hello World!</p>
     </div>
   )

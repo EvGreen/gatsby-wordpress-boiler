54a9f357b6929d94797df77dd3ef73ca87586dfc
Daniel Leczycki <the242@gmail.com>
2020-04-10 10:06:47 +0200

    Moving google fonts to Helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Layout.scss"
+import { Helmet } from "react-helmet"
 
 import { ParallaxProvider } from "react-scroll-parallax"
 
@@ -11,6 +12,19 @@ function Layout({ children, location }) {
 
   return (
   	<>
+			<Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Open+Sans:ital,wght@300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" />
+				<link rel="stylesheet"
+							href="https://fonts.googleapis.com/css?family=Quicksand:wght@300;400;500;600;700&display=swap" />
+				<link rel="stylesheet"
+							href="https://fonts.googleapis.com/css?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
+      </Helmet>
+			
 			<Header />
 			
 			{/* <AnimatePresence exitBeforeEnter> */}

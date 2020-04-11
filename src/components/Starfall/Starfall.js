6496b1e385f459c2d1ee6433c62fb21a42e6c61c
Daniel Leczycki <the242@gmail.com>
2020-04-11 21:34:24 +0200

    Starfal fade

--- src/components/Starfall/Starfall.js
+++ src/components/Starfall/Starfall.js
@@ -12,10 +12,11 @@ function getRandomInt(min, max) {
 const starFall = item => {
 	var randomDurationIntro = getRandomInt(200,300)
 	var randomDurationOutro = getRandomInt(200,300)
-	var randomDelayIntro = getRandomInt(300,520)
-	var randomDelayOutro = getRandomInt(150,350)
+	var randomDelayIntro = getRandomInt(100,220)
+	var randomDelayOutro = getRandomInt(150,250)
 	anime.timeline({
 		targets: item,
+		opacity: [1,0.05],
 		easing: 'easeInOutSine',
 		loop: true
 	})
@@ -36,25 +37,27 @@ const starFall = item => {
 function Starfall(props) {	
 
 	useEffect(() => {
-		const startrails = document.getElementsByClassName("startrail")
-		for (var i = 0; i < startrails.length; i++) {
-			starFall(startrails[i])
-		}
+		setTimeout(() => {
+			const startrails = document.getElementsByClassName("startrail")
+			for (var i = 0; i < startrails.length; i++) {
+				starFall(startrails[i])
+			}
+		}, 1500);
 	},[])
 
 	return (
 		<div className="starfall">
 			<div className="star" style={{ top: "30%", left: "20%", width: "2px", height: "5em" }}>
-				<div className="startrail"></div>
+				<div className="startrail" style={{ opacity: 0 }}></div>
 			</div>
 			<div className="star" style={{ top: "40%", left: "40%", width: "2px", height: "7em" }}>
-				<div className="startrail"></div>
+				<div className="startrail" style={{ opacity: 0 }}></div>
 			</div>
 			<div className="star" style={{ top: "50%", left: "60%", width: "2px", height: "8em" }}>
-				<div className="startrail"></div>
+				<div className="startrail" style={{ opacity: 0 }}></div>
 			</div>
 			<div className="star" style={{ top: "35%", left: "80%", width: "2px", height: "4em" }}>
-				<div className="startrail"></div>
+				<div className="startrail" style={{ opacity: 0 }}></div>
 			</div>
 		</div>
   )

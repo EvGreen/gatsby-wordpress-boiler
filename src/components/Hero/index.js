698622a72a864a8d022dd7aaadc8c3df554b2d07
Daniel Leczycki <the242@gmail.com>
2020-04-22 19:48:49 +0200

    Div100vh

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import Img from 'gatsby-image'
 import anime from 'animejs'
 
+import Div100vh from 'react-div-100vh'
+
 //import PropTypes from 'prop-types'
 import './style.scss'	
 
@@ -20,28 +22,30 @@ function Hero(props) {
 	},[])
 
 	return (
-		<section ref={props.heroRef} className='hero x0 t'>
-			
-			{ image ?
-				<Img fluid={image}
-					imgStyle={{objectFit: 'cover'}}
-					objectPosition='50% 50%'
-					loading='eager'
-					fadeIn={true}
-				/>
-			: null }
+		<Div100vh>
+			<section ref={props.heroRef} className='hero x0 t'>
+				
+				{ image ?
+					<Img fluid={image}
+						imgStyle={{objectFit: 'cover'}}
+						objectPosition='50% 50%'
+						loading='eager'
+						fadeIn={true}
+					/>
+				: null }
 
-			<div className='bg-overlay'>
-				<div className='bg-overlay-x'></div>
-			</div>
-
-			<div className='hero-content'>
-				<div className="hero-content-animator" style={{opacity: 0}}>
-					<div className='hero-content-box' dangerouslySetInnerHTML={{__html: content}} />
+				<div className='bg-overlay'>
+					<div className='bg-overlay-x'></div>
 				</div>
-			</div>
 
-		</section>
+				<div className='hero-content'>
+					<div className="hero-content-animator" style={{opacity: 0}}>
+						<div className='hero-content-box' dangerouslySetInnerHTML={{__html: content}} />
+					</div>
+				</div>
+
+			</section>
+		</Div100vh>
   )
 }
 

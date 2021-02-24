338ff4fed31714cfea07634698b98cefaca20d9f
Daniel Leczycki <the242@gmail.com>
2021-02-24 17:20:24 +0100

    automating some custom stuff

--- src/components/Content/ACF/Custom/HeroDetail/index.js
+++ src/components/Content/ACF/Custom/HeroDetail/index.js
@@ -3,59 +3,43 @@ import './style.scss'
 
 import Img from 'gatsby-image'
 
-function HeroDetail({images, files, fields, wysiwygs, maps}) {
-
-	// Trzeba to zautomatyzwoac
-	const img0 = images[0]?.asset_img?.localFile.childImageSharp.fluid	
-	const img1 = images[1]?.asset_img?.localFile.childImageSharp.fluid	
-	const img2 = images[2]?.asset_img?.localFile.childImageSharp.fluid	
-	const img3 = images[3]?.asset_img?.localFile.childImageSharp.fluid
-
-	const field0 = fields[0]?.asset_field
-	const field1 = fields[1]?.asset_field
-	const field2 = fields[2]?.asset_field
-	const field3 = fields[3]?.asset_field
-	const field4 = fields[4]?.asset_field
-	const field5 = fields[5]?.asset_field
-	const field6 = fields[6]?.asset_field
-	const field7 = fields[7]?.asset_field
-	const field8 = fields[8]?.asset_field
+function HeroDetail({images: img, files: file, fields: field, wysiwygs: wysiwyg, maps: map}) {
 
 	return (
 		<>
-			{field0}
-			{field1}
-			{field2}
-			{field3}
-			{field4}
-			{field5}
-			{field6}
-			{field7}
-			{field8}
+			{field[0]}
+			{field[1]}
+			{field[2]}
+			{field[3]}
+			{field[4]}
+			{field[5]}
+			{field[6]}
+			{field[7]}
+			{field[8]}
 
-			{ img0 ?
-				<Img fluid={img0}
+			{ img[0] ?
+				<Img fluid={img[0]}
 					imgStyle={{objectFit: 'cover'}}
 					objectPosition='50% 50%'
 					className='img1'
 				/>
 			: null }
-			{ img1 ?
-				<Img fluid={img1}
+			{ img[1] ?
+				<Img fluid={img[1]}
 					imgStyle={{objectFit: 'cover'}}
 					objectPosition='50% 50%'
 					className='img1'
 				/>
 			: null }
-			{ img2 ?
-				<Img fluid={img2}
+			{ img[2] ?
+				<Img fluid={img[2]}
 					imgStyle={{objectFit: 'cover'}}
 					objectPosition='50% 50%'
 					className='img1'
 				/>
 			: null }
-			{ img3 ?
-				<Img fluid={img3}
+			{ img[3] ?
+				<Img fluid={img[3]}
 					imgStyle={{objectFit: 'cover'}}
 					objectPosition='50% 50%'
 					className='img1'
--- src/components/Content/ACF/Custom/index.js
+++ src/components/Content/ACF/Custom/index.js
@@ -7,18 +7,33 @@ function ACFCustom(props) {
 
 	// Images	
 	const images = props.asset?.filter(obj => obj.acf_fc_layout == 'image')
+		.map((node,i) => {
+			return node.asset_img?.localFile.childImageSharp.fluid
+		})
 	
 	// Files	
 	const files = props.asset?.filter(obj => obj.acf_fc_layout == 'file')
+		.map((node,i) => {
+			return node.asset_file
+		})
 
 	// Fields	
 	const fields = props.asset?.filter(obj => obj.acf_fc_layout == 'field')
+		.map((node,i) => {
+			return node.asset_field
+		})
 
 	// WYSIWYGS	
 	const wysiwygs = props.asset?.filter(obj => obj.acf_fc_layout == 'wysiwyg')
+		.map((node,i) => {
+			return node.asset_wysiwyg
+		})
 
 	// Maps	
 	const maps = props.asset?.filter(obj => obj.acf_fc_layout == 'map')
+		.map((node,i) => {
+			return node.asset_map
+		})
 
 
 	if (props.acf_fc_layout === 'custom' && props.anchor === 'custom-home-detail') {

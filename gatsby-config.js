cc340795d9371f40f82c190974be7a80b27a9baa
Daniel Leczycki <the242@gmail.com>
2021-03-02 18:33:15 +0100

    updates huh

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify-cache`,
     `gatsby-plugin-netlify`, // make sure to keep it last in the array
   ],
 }

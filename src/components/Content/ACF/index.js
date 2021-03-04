53cc7d3519f108e9b59cebb5989c0692a8ac2cec
Daniel Leczycki <the242@gmail.com>
2021-03-04 10:16:22 +0100

    little cleanup

--- src/components/Content/ACF/index.js
+++ src/components/Content/ACF/index.js
@@ -8,8 +8,6 @@ import ACFWYSIWYG from './WYSIWYG'
 import ACFCustom from './Custom'
 
 import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 
 // Slider settings
 const sliderSettings = {

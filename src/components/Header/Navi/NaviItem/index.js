618fa5e10c9304beacae6695a60105e1695511b5
Daniel Leczycki <the242@gmail.com>
2021-08-03 09:38:36 +0200

    url validation regex

--- src/components/Header/Navi/NaviItem/index.js
+++ src/components/Header/Navi/NaviItem/index.js
@@ -9,6 +9,16 @@ import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 import { Link } from 'gatsby'
 
+function validURL(str) {
+	var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+		'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+		'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+		'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+	return !!pattern.test(str);
+}
+
 function NaviItem(props) {
 	const naviContext = useContext(NaviContext)
 
